b0f23c4d1cbfbe3e8fe3396c27c467667f8d4a78
Prasheel-IITI <70912843+Prasheel-IITI@users.noreply.github.com>
2021-11-14 15:49:03 +0530

    Adding Timer Output

--- Algorithm4/main.cpp
+++ Algorithm4/main.cpp
@@ -7,6 +7,7 @@ int main()
 {
     Graph g;
     g.readGraph();
+    auto start = std::chrono::high_resolution_clock::now();
 #pragma omp parallel for schedule(dynamic, g.m)
     for (int i = 0; i < g.dst.size(); ++i)
     {
@@ -14,12 +15,16 @@ int main()
         int u = FindSrc(g.offset, g.m, i);
         g.cnt[i] = ComputeCntBMP(g, u, v);
     }
-
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    int timeTaken = duration.count();
     for (int i = 0; i < g.dst.size(); ++i)
     {
         int v = g.dst[i];
         int u = FindSrc(g.offset, g.m, i);
         cout << u + 1 << ' ' << v + 1 << ' ' << g.cnt[i] << "\n";
     }
+    cout << "Time Taken=" << timeTaken << " microseconds" << endl;
+
     return 0;
 }
