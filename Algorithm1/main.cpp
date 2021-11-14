2189bad41313ace33d08569b4a11ee3c6017c007
Prasheel-IITI <70912843+Prasheel-IITI@users.noreply.github.com>
2021-11-14 15:42:30 +0530

    Fixing Initial Errors

--- Algorithm1/main.cpp
+++ Algorithm1/main.cpp
@@ -5,25 +5,34 @@ using namespace std;
 
 int main()
 {
-
     Graph g;
     g.readGraph();
 
+    auto start = std::chrono::high_resolution_clock::now();
     int p = 0, v;
-    for (int i = 0; i < g.n; ++i) {
-        while (p < g.offset[i + 1]) {
+    for (int i = 0; i < g.n; ++i)
+    {
+        while (p < g.offset[i + 1])
+        {
             vector<int> A, B;
-            for (int j = g.offset[i]; j < g.offset[i + 1]; ++j) {
+            for (int j = g.offset[i]; j < g.offset[i + 1]; ++j)
+            {
                 A.push_back(g.dst[j]);
             }
             v = g.dst[p];
-            for (int j = g.offset[v]; j < g.offset[v + 1]; ++j) {
-                A.push_back(g.dst[j]);
+            for (int j = g.offset[v]; j < g.offset[v + 1]; ++j)
+            {
+                B.push_back(g.dst[j]);
             }
-            g.cnt[i] = intersectMPS(A, B);
-            cout << g.cnt[i] << "\n";
+            g.cnt[p] = intersectMPS(A, B);
+            cout << g.cnt[p] << "\n";
+            p++;
         }
     }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    int timeTaken = duration.count();
+    cout << "time taken= " << timeTaken << " microseconds\n";
 
     return 0;
 }
