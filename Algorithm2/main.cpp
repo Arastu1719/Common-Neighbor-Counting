109e61bb8db2ed44608619ae3457302b2c51a5c5
Prasheel-IITI <70912843+Prasheel-IITI@users.noreply.github.com>
2021-11-14 15:43:16 +0530

    Fixing Initial Errors

--- Algorithm2/main.cpp
+++ Algorithm2/main.cpp
@@ -6,19 +6,24 @@ int main()
 {
     Graph g;
     g.readGraph();
+    auto start = std::chrono::high_resolution_clock::now();
     vector<bool> B(g.n);
     int v;
 
     int p = 0;
-    for (int i = 0; i < g.n; ++i) {
-        for (int j = g.offset[i]; j < g.offset[i + 1]; ++j) {
+    for (int i = 0; i < g.n; ++i)
+    {
+        for (int j = g.offset[i]; j < g.offset[i + 1]; ++j)
+        {
             B[g.dst[j]] = true;
         }
 
-        while (p < g.offset[i + 1]) {
+        while (p < g.offset[i + 1])
+        {
             vector<int> A;
             v = g.dst[p];
-            for (int k = g.offset[v]; k < g.offset[v + 1]; ++k) {
+            for (int k = g.offset[v]; k < g.offset[v + 1]; ++k)
+            {
                 A.push_back(g.dst[k]);
             }
 
@@ -26,12 +31,18 @@ int main()
             ++p;
         }
 
-        for (int j = g.offset[i]; j < (i == g.n - 1 ? g.dst.size() : g.offset[i + 1]); ++j) {
+        for (int j = g.offset[i]; j < (i == g.n - 1 ? g.dst.size() : g.offset[i + 1]); ++j)
+        {
             B[g.dst[j]] = false;
         }
     }
-    for (int i = 0; i < g.m; ++i) {
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    int timeTaken = duration.count();
+    for (int i = 0; i < g.m; ++i)
+    {
         cout << g.cnt[i] << "\n";
     }
+    cout << "time taken= " << timeTaken << " microseconds\n";
     return 0;
 }
