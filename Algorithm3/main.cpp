7175b1474057f82b490671fb14153bf134103e83
Prasheel-IITI <70912843+Prasheel-IITI@users.noreply.github.com>
2021-11-14 15:48:25 +0530

    Adding Timer

--- Algorithm3/main.cpp
+++ Algorithm3/main.cpp
@@ -7,7 +7,8 @@ int main()
 {
     Graph g;
     g.readGraph();
-    #pragma omp parallel for schedule(dynamic, g.m)
+    auto start = std::chrono::high_resolution_clock::now();
+#pragma omp parallel for schedule(dynamic, g.m)
     for (int i = 0; i < g.dst.size(); ++i)
     {
         int v = g.dst[i];
@@ -23,12 +24,16 @@ int main()
         }
         g.cnt[i] = ComputeCntMPS(A, B);
     }
-
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    int timeTaken = duration.count();
     for (int i = 0; i < g.dst.size(); ++i)
     {
         int v = g.dst[i];
         int u = FindSrc(g.offset, g.m, i);
         cout << u + 1 << ' ' << v + 1 << ' ' << g.cnt[i] << "\n";
     }
+    cout << "Time Taken=" << timeTaken << " microseconds" << endl;
+
     return 0;
 }
